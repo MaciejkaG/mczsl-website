957ba983fd006e37bcdc160553a160189f6244ef
MaciejkaG <me@maciejka.xyz>
2024-05-14 21:25:10 +0200

    Added mini team and testing different screenshot view

--- src/components/screenshots.tsx
+++ src/components/screenshots.tsx
@@ -14,21 +14,24 @@ export default function Screenshots() {
   ];
 
   return (
-    <div className="hidden md:block relative w-full overflow-hidden my-12">
-      {/* Left fade */}
-      <div className="absolute left-0 w-2/12 h-full bg-gradient-to-r from-background to-transparent z-10" />
-      {/* Right fade */}
-      <div className="absolute right-0 w-2/12 h-full bg-gradient-to-l from-background to-transparent z-10" />
+    // <div className="hidden md:block relative w-full overflow-hidden my-12">
+    //   {/* Left fade */}
+    //   <div className="absolute left-0 w-2/12 h-full bg-gradient-to-r from-background to-transparent z-10" />
+    //   {/* Right fade */}
+    //   <div className="absolute right-0 w-2/12 h-full bg-gradient-to-l from-background to-transparent z-10" />
 
-      {/* Actual content */}
-      <div className={`flex w-full h-full bg-background ${styles.animationContainer}`}>
-        {screenshots.map((url, index) =>
+    //   {/* Actual content */}
+    //   <div className={`flex w-full h-full bg-background ${styles.animationContainer}`}>
+    //     {screenshots.map((url, index) =>
 
-          <div className="slide-item w-1/4 h-full flex-shrink-0" key={index}>
-            <Image src={url} alt="Server screenshot" height={720} width={1280} className="w-full" />
-          </div>
-        )}
-      </div>
+    //       <div className="slide-item w-1/4 h-full flex-shrink-0" key={index}>
+    //         <Image src={url} alt="Server screenshot" width={1280} height={720} className="w-full" />
+    //       </div>
+    //     )}
+    //   </div>
+    // </div>
+    <div className="w-full h-80 overflow-hidden flex justify-center items-center my-12">
+      <Image src="/screenshot1.png" alt="Server screenshot" width={1280} height={720} className="object-cover min-w-full min-h-full" />
     </div>
   );
 }
--- src/components/team.tsx
+++ src/components/team.tsx
@@ -3,8 +3,18 @@ import Image from "next/image";
 function TeamMember(props: { name: string, description: string, imgUrl: string,  }) {
   return (
     <div className="w-full sm:w-1/3 p-6 box-border text-center flex justify-center items-center flex-col">
-      <Image src={props.imgUrl} alt="Member image" width={256} height={256} className="w-1/2 rounded-full" />
+      <Image src={props.imgUrl} alt="Member image" width={512} height={512} className="w-1/2 rounded-full" />
       <h2>{props.name}</h2>
+      <span>{props.description}</span>
+    </div>
+  )
+}
+
+function SmallTeamMember(props: { name: string, description: string, imgUrl: string, }) {
+  return (
+    <div className="w-1/2 sm:w-1/3 lg:w-1/5 max-w-[33vw] p-2 sm:p-4 box-border text-center flex justify-center items-center flex-col">
+      <Image src={props.imgUrl} alt="Member image" width={256} height={256} className="w-2/3 rounded-full" />
+      <h2 className="text-xl">{props.name}</h2>
       <span>{props.description}</span>
     </div>
   )
@@ -14,10 +24,21 @@ export default function Team() {
   return (
     <div className="flex w-full items-center text-center flex-col">
       <h1 className="text-4xl mb-8">Poznaj administrację</h1>
+      {/* Head admins (bigger gallery) */}
       <div className="flex w-10/12 sm:w-3/4 justify-center items-center flex-col sm:flex-row">
         <TeamMember imgUrl="/mcjk-logo.png" name="Maciejka" description="Designer / Deweloper / Zarządca Discorda / Zarządca infrastruktury" />
         <TeamMember imgUrl="/pingwinek.webp" name="Pingwinek" description="Zarządca infrastruktury / Technik serwera Minecraft" />
         <TeamMember imgUrl="/pingwinek.webp" name="Atoseczek" description="Zarządca Discorda / Technik serwera Minecraft" />
+      </div>
+      {/* Other people helping the server */}
+      <div className="flex w-11/12 sm:2-3/4 justify-center flex-row flex-wrap">
+        <SmallTeamMember imgUrl="/mcjk-logo.png" name="To jest test" description="Moderator Discorda" />
+        <SmallTeamMember imgUrl="/mcjk-logo.png" name="To jest test" description="Moderator Discorda" />
+        <SmallTeamMember imgUrl="/mcjk-logo.png" name="To jest test" description="Moderator Discorda" />
+        <SmallTeamMember imgUrl="/mcjk-logo.png" name="To jest test" description="Moderator Discorda" />
+        <SmallTeamMember imgUrl="/mcjk-logo.png" name="To jest test" description="Moderator Discorda" />
+        <SmallTeamMember imgUrl="/mcjk-logo.png" name="To jest test" description="Moderator Discorda" />
+        <SmallTeamMember imgUrl="/mcjk-logo.png" name="To jest test" description="Moderator Discorda" />
       </div>
     </div>
   );
